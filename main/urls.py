d5ef7d2c41e3cdb7a0bfad223abce20376b925da
Bradley Shade <bshade@enengineering.com>
2018-12-11 21:33:24 -0600

    Renamed files to match convention

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ from main.programs import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^programs/$', views.book_list, name='program_list'),
-    url(r'^programs/create/$', views.book_create, name='program_create'),
-    url(r'^programs/(?P<pk>\d+)/update/$', views.book_update, name='program_update'),
-    url(r'^programs/(?P<pk>\d+)/delete/$', views.book_delete, name='program_delete'),
+    url(r'^programs/$', views.program_list, name='program_list'),
+    url(r'^programs/create/$', views.program_create, name='program_create'),
+    url(r'^programs/(?P<pk>\d+)/update/$', views.program_update, name='program_update'),
+    url(r'^programs/(?P<pk>\d+)/delete/$', views.program_delete, name='program_delete'),
 ]
